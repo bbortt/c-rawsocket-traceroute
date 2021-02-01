a0c7f0cbf18a8f10bf9b635c3ea50535faa45f2c
Timon Borter <timon.borter@gmx.ch>
2021-02-01 16:28:40 +0100

    trace including host name lookup.

--- traceroute.c
+++ traceroute.c
@@ -15,6 +15,7 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <limits.h>
 
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
@@ -23,6 +24,11 @@
 //#include <sys/cdefs.h>
 //#include <sys/types.h>
 //#include <linux/ip.h>
+
+#include "crc.c"
+
+#define MAX_HOPS 30
+#define IP_IDENTIFICATION 12345
 
 struct trace_span {
     uint8_t ttl;
@@ -35,21 +41,20 @@ int get_if_addr(const char *if_name, struct in_addr *src) {
 
     struct ifaddrs *addrs;
     getifaddrs(&addrs);
-    struct ifaddrs *tmp = addrs;
-    while (tmp) {
-        if (tmp->ifa_addr && tmp->ifa_addr->sa_family == AF_INET) {
-            struct sockaddr_in *if_addr = (struct sockaddr_in *) tmp->ifa_addr;
-            if (0 == strcmp(tmp->ifa_name, if_name)) {
-                memcpy(src, inet_ntoa(if_addr->sin_addr), sizeof(struct in_addr));
+    struct ifaddrs *next_addr = addrs;
+    while (next_addr) {
+        if (next_addr->ifa_addr && next_addr->ifa_addr->sa_family == AF_INET) {
+            struct sockaddr_in *if_addr = (struct sockaddr_in *) next_addr->ifa_addr;
+            if (0 == strcmp(next_addr->ifa_name, if_name)) {
+                memcpy(src, &if_addr->sin_addr, sizeof(struct in_addr));
                 freeifaddrs(addrs);
                 return 0;
             }
         }
 
-        tmp = tmp->ifa_next;
+        next_addr = next_addr->ifa_next;
     }
 
-    freeifaddrs(addrs);
     return -1;
 }
 
@@ -76,14 +81,6 @@ int hostname_to_ip(char *hostname, struct in_addr *dest) {
     return -1;
 }
 
-int16_t calc_checksum(const uint16_t *data, unsigned int bytes) {
-    uint32_t sum = 0;
-    for (unsigned int i = 0; i < bytes / 2; i++)
-        sum += data[i];
-    sum = (sum & 0xffff) + (sum >> 16);
-    return htons(0xffff - sum);
-}
-
 char *build_span(const struct trace_span *span) {
     // build packet
     char *packet = malloc(sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct udphdr));
@@ -98,22 +95,23 @@ char *build_span(const struct trace_span *span) {
     iph->version = 4;
     iph->tos = 0;
     iph->tot_len = sizeof(struct iphdr) + sizeof(struct icmphdr);
-    iph->id = htons(35897);
-    iph->frag_off = htons(12345);
+    iph->id = htons(IP_IDENTIFICATION);
+    iph->frag_off = 0;
     iph->ttl = span->ttl;
     iph->protocol = IPPROTO_ICMP;
-    iph->check = 0;
     iph->saddr = (uint32_t) span->src->s_addr;
     iph->daddr = (uint32_t) span->dest->s_addr;
+    iph->check = 0;
 
     // create ICMP header
     icmphd->type = ICMP_ECHO;
     icmphd->code = 0;
     icmphd->checksum = 0;
-    icmphd->checksum = calc_checksum((unsigned short *) (packet + sizeof(struct iphdr)), 4);
+    icmphd->checksum = GNUNET_CRYPTO_crc16_n((unsigned short *) (packet + sizeof(struct iphdr)),
+                                             sizeof(struct icmphdr));
 
-    // calculate IP checksum
-    iph->check = calc_checksum((unsigned short *) packet, iph->tot_len >> 1);
+    // Calculate full checksumn
+    iph->check = GNUNET_CRYPTO_crc16_n((unsigned short *) packet, sizeof(struct iphdr) + sizeof(struct icmphdr));
 
     // create UDP header
     struct udphdr *udp = (struct udphdr *) (packet + sizeof(struct iphdr) + sizeof(struct icmphdr));
@@ -121,7 +119,8 @@ char *build_span(const struct trace_span *span) {
     udp->dest = (63000);
     udp->len = htons(8);
     udp->check = 0;
-    udp->check = calc_checksum((unsigned short *) (packet + sizeof(struct iphdr) + sizeof(struct icmphdr)), 4);
+    udp->check = GNUNET_CRYPTO_crc16_n((unsigned short *) (packet + sizeof(struct iphdr) + sizeof(struct icmphdr)),
+                                       sizeof(struct udphdr));
 
     return packet;
 }
@@ -147,6 +146,10 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char if_ip[15];
+    inet_ntop(AF_INET, &src, if_ip, sizeof(if_ip));
+    printf("Will fire from networking interface %s\n", if_ip);
+
     // check and resolve destination hostname
     char *hostname = argv[1];
     struct in_addr dest;
@@ -161,44 +164,84 @@ int main(int argc, char **argv) {
 
     printf("tracing down '%s' on '%s'..\n", hostname, ip);
 
-    // finally bind raw socket
-    int on = 1;
+    // finally bind raw socket, OUTBOUND
+    int on = 1; // we include headers in the packet
     int out_sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (0 > out_sock) {
-        fprintf(stderr, "Error creating raw socket!\n");
+        fprintf(stderr, "Error creating raw out-socket!\n");
         return -1;
     }
     if (0 != setsockopt(out_sock, IPPROTO_IP, IP_HDRINCL, (const char *) &on, sizeof(on))) {
-        fprintf(stderr, "Error setting socket options!\n");
+        fprintf(stderr, "Error setting out-socket options!\n");
         return -1;
     }
 
-    // build next raceroute span
+    // bind raw socket, INBOUND
+    int in_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (0 > in_sock) {
+        fprintf(stderr, "Error creating raw in-socket!\n");
+        return -1;
+    }
+    if (0 != setsockopt(in_sock, IPPROTO_IP, IP_HDRINCL, (const char *) &on, sizeof(on))) {
+        fprintf(stderr, "Error setting in-socket options!\n");
+        return -1;
+    }
+
+    // build traceroute span
     struct trace_span span;
-    span.ttl = 1;
     span.src = &src;
     span.dest = &dest;
 
-    char *packet = build_span(&span);
-
+    // destination socket address
     struct sockaddr_in dest_sockaddr;
     dest_sockaddr.sin_family = AF_INET;
     dest_sockaddr.sin_addr.s_addr = dest.s_addr;
     dest_sockaddr.sin_port = htons(80);
 
-    // send span
-    printf("Sending span %d..\n", span.ttl);
-    ssize_t dat = sendto(out_sock, packet, sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct udphdr), 0,
-                         (struct sockaddr *) &dest_sockaddr, sizeof dest_sockaddr);
-    printf("Sent! %d\n", (int) dat);
+    for (uint8_t i = 0; i < MAX_HOPS; i++) {
+        // upgrade span, create packet
+        span.ttl = i + 1;
+        char *packet = build_span(&span);
 
-    free(packet);
+        // send span
+        printf("Sending span %d..\n", span.ttl);
+        ssize_t dat = sendto(out_sock, packet, sizeof(struct iphdr) + sizeof(struct icmphdr) + sizeof(struct udphdr), 0,
+                             (struct sockaddr *) &dest_sockaddr, sizeof dest_sockaddr);
+        printf("Sent %d byte span\n", (int) dat);
 
-    // receive packet
-//    int in_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-//    setsockopt(in_sock, IPPROTO_IP, IP_HDRINCL, (const char *) &on, sizeof(on));
-//    char buf[65536];
-//    ssizet_t ret = read(icmpsock, buf, sizeof(buf));
+        free(packet);
+
+        // receive answer
+        struct sockaddr_in receiver;
+        socklen_t sockaddr_size = sizeof(receiver);
+        char buf[UINT16_MAX];
+        recvfrom(in_sock, buf, sizeof(struct iphdr) + sizeof(struct icmphdr), 0, (struct sockaddr *) &receiver,
+                 &sockaddr_size);
+        struct iphdr *ip = (struct iphdr *) buf;
+        struct icmphdr *icmp = (struct icmphdr *) (buf + sizeof(struct iphdr));
+
+        // "Translate" response ip
+        char res_ip[15];
+        inet_ntop(AF_INET, &ip->saddr, res_ip, sizeof(res_ip));
+
+        // Lookup hostname by response ip
+        struct sockaddr_in lkp;
+        memset(&lkp, 0, sizeof(lkp));
+        lkp.sin_family = AF_INET;
+        lkp.sin_addr.s_addr = ip->saddr;
+
+        static char hostname[65];
+        if (0 != getnameinfo((struct sockaddr *) &lkp, sizeof(lkp), hostname, sizeof(hostname), NULL, 0, 0)) {
+            strcpy(hostname, "*");
+        }
+
+        printf("Answer %d from [%s]: %s\n", span.ttl, hostname, res_ip);
+
+        if (0 == memcmp(&dest, &ip->saddr, sizeof(struct in_addr))) {
+            printf("Trace complete!\n");
+            i = MAX_HOPS;
+        }
+    }
 
     return 0;
 }
